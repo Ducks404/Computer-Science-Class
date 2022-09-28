142a4becbf88f43e35da69780a5fbefe3a3ec319
Ducks404 <aghakoeswantoro@gmail.com>
2022-09-29 01:20:05 +0700

    Task 2

--- ProjectParking.py
+++ ProjectParking.py
@@ -7,21 +7,29 @@ for day in range(14):
 
 #print(booking)
 
-def checkspace(day):
+def checkspace(day, accesible):
     space = -1
-    for index, i in enumerate(day):
-        if i == []:
-            space = index
+    if accesible == 'n':
+        for index in range(6,20)[::-1]:
+            if day[index] == []:
+                space = index
+                break
+    elif accesible == 'y':
+        for index in range(20):
+            if day[index] == []:
+                space = index
+                break
     return space
 
 # pass to loop back to asking
 n=2
 while n>0:
     day = int(input("On what day would you like to book? (1-14) "))
+    accesible = input("Do you need an accesible spot? (Y/N) ").lower()
 
-    if day >= 1 and day <= 14:
+    if day >= 1 and day <= 14 and (accesible == 'y' or accesible == 'n'):
         day-=1
-        space = checkspace(booking[day])
+        space = checkspace(booking[day], accesible)
         if space == -1:
             print("Sorry, unavailable day")
         else:
