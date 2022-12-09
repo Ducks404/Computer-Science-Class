ca59775c7a18a69169a4514307e10b958e5ebef6
Ducks404 <aghakoeswantoro@gmail.com>
2022-12-10 00:01:57 +0700

    redundant line

--- parking task3.py
+++ parking task3.py
@@ -83,7 +83,6 @@ while not done:
 
     # admin page
     if day == "admin":
-        choice = -1
         while True:
             print("1. Number of accessible spaces used on inputted day")
             print("2. Number of general spaces used on inputted day")
